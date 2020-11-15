60818204c8f95ed6e410e861b63fdf2fcd73de9f
markuspinter <markus.m.pinter@gmail.com>
2020-11-15 19:49:53 +0100

    open dialog when activating igniter

--- client/js/ClientSocket.js
+++ client/js/ClientSocket.js
@@ -53,7 +53,50 @@ chartTabObserver.observe(document.getElementById('chart-tab'), {
   attributes: true //configure it to listen to attribute changes
 });
 
+var lastModalTriggeredElement = undefined;
+
 //-------------------------------------GUI Events---------------------------------
+
+$('#outputAssuranceModal').on('show.bs.modal', function (event) {
+        var relatedTarget = $(event.relatedTarget); // Button that triggered the modal
+        lastModalTriggeredElement = event.relatedTarget;
+      // var recipient = button.data('whatever') // Extract info from data-* attributes
+      // // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+      // // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+      // var modal = $(this)
+      // modal.find('.modal-title').text('New message to ' + recipient)
+      // modal.find('.modal-body input').val(recipient)
+});
+
+function onCheckboxModal(checkbox)
+{
+    if (checkbox.checked)
+    {
+        $(checkbox).prop('checked', false);
+        $('#outputAssuranceModal').modal('show', checkbox);
+    }
+    else
+    {
+        onDigitalCheck(checkbox);
+    }
+}
+
+function onModalAccept() {
+    console.log(lastModalTriggeredElement);
+    if (lastModalTriggeredElement !== undefined)
+    {
+        if ($(lastModalTriggeredElement).hasClass("digitalOut"))
+        {
+            $(lastModalTriggeredElement).prop('checked', true);
+            onDigitalCheck(lastModalTriggeredElement);
+        }
+    }
+}
+
+function onModalReject()
+{
+
+}
 
 function onResetSensors() {
     emptySensorCharts();
