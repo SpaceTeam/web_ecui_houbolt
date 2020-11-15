f49bdd38e0f2c172804579f1a6ee58e025d5a0fa
markuspinter <markus.m.pinter@gmail.com>
2020-11-15 20:05:24 +0100

    show actuator name in modal

--- client/js/ClientSocket.js
+++ client/js/ClientSocket.js
@@ -58,14 +58,9 @@ var lastModalTriggeredElement = undefined;
 //-------------------------------------GUI Events---------------------------------
 
 $('#outputAssuranceModal').on('show.bs.modal', function (event) {
-        var relatedTarget = $(event.relatedTarget); // Button that triggered the modal
-        lastModalTriggeredElement = event.relatedTarget;
-      // var recipient = button.data('whatever') // Extract info from data-* attributes
-      // // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-      // var modal = $(this)
-      // modal.find('.modal-title').text('New message to ' + recipient)
-      // modal.find('.modal-body input').val(recipient)
+    lastModalTriggeredElement = event.relatedTarget;
+    var modal = $(this)
+    modal.find('#modal-output-name').text(event.relatedTarget.id);
 });
 
 function onCheckboxModal(checkbox)
