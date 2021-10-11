9fd0181db3f5e34ced4c0b09ab0f5a5a2c011979
LuPro <luis.immanuel@gmx.at>
2021-10-11 16:00:58 +0200

    update theme to be more coherent and work better with theme changes updated logo to use a white and black variant based on theme instead of invert effect moved some theme handling out of the theme files (to only contain color info) into styles.css (status bar coloring)

--- client/js/WebECUI.js
+++ client/js/WebECUI.js
@@ -1,7 +1,25 @@
 function initWebECUI()
 {
     let themeSwitcherContainer = $(`<div class="themeSwitcher"></div>`).appendTo($(document.body));
-    initThemes(themeSwitcherContainer, "theming/", [{theme: "lightTheme", icon: "brightness-high", type: "light"},{theme: "darkTheme", icon: "moon", type: "dark"}]);
+    initThemes(themeSwitcherContainer, "theming/", [{theme: "darkTheme", icon: "moon", type: "dark"},{theme: "lightTheme", icon: "brightness-high", type: "light"}]);
+
+    themeSubscribe($("#icon"), function(event){logoThemeHandler(event)});
+}
+
+function logoThemeHandler(event)
+{
+    if (event.detail["type"] == "light")
+    {
+        $("#icon").attr("src", "img/Space_Team_Logo_black.png");
+    }
+    else if (event.detail["type"] == "dark")
+    {
+        $("#icon").attr("src", "img/Space_Team_Logo_white.png");
+    }
+    else
+    {
+        printLog("info", `Tried setting logo type that doesn't exist: ${type}`);
+    }
 }
 
 initWebECUI();
