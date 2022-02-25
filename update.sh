cb2f686bd8d4a1482bb7bede950c3d9f249d5faa
Franky333 <1.daniel.frank@gmail.com>
2022-02-25 14:12:32 +0100

    added gss config

--- update.sh
+++ update.sh
@@ -5,5 +5,6 @@ cd "$(dirname "$0")"
 git pull
 
 #sudo systemctl restart ecui-web-large-teststand.service
-sudo systemctl restart ecui-web-small-teststand.service
+#sudo systemctl restart ecui-web-small-teststand.service
 #sudo systemctl restart ecui-web-small-oxfill.service
+sudo systemctl restart ecui-web-gss.service
