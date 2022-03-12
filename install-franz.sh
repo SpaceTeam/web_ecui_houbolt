618c7c34a00c86aa7eeb50da2cf5137b2bce2253
markuspinter <markus.m.pinter@gmail.com>
2022-03-12 15:36:38 +0100

    add install and service script, update pnid

---
+++ install-franz.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 12.10.0
+npm install
+
+sudo cp ecui-web-franz.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable ecui-web-franz.service
+sudo systemctl start ecui-web-franz.service
+sudo systemctl status ecui-web-franz.service
+
+sudo chmod +x update.sh
+./update.sh
+
+
